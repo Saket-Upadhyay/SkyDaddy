44f5404fd3a227118ef85b215cfe5537531468a7
Saket-Upadhyay <saketupadhyay@protonmail.com>
2023-03-06 18:05:37 -0500

    shell fix

--- serve_docker.sh
+++ serve_docker.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env sh
-docker compose up "$*" -d --scale skydaddy=1
+
+docker volume create sharedUPLOAD
+docker volume create sharedPERMA
+
+if [ "$*" != "" ]; then
+  docker compose up -d "$*" --scale skydaddy=1
+else
+  docker compose up -d --scale skydaddy=1
+fi
+
